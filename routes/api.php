fa6642ad44356c7035ab2a201c6e8347cef09fc5
Belle Ame <umairsaif.pk@gmail.com>
2024-11-01 23:27:39 +0500

    Routes Added!

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\ChatroomController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +17,28 @@ Route::post('/login', [AuthenticatedSessionController::class, 'store'])
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::prefix('chatrooms')->group(function () {
+        Route::post('/', [ChatroomController::class, 'createChatroom'])
+            ->name('chatrooms.create');
+
+        Route::get('/', [ChatroomController::class, 'listChatrooms'])
+            ->name('chatrooms.list');
+
+        Route::post('{chatroomId}/enter', [ChatroomController::class, 'enterChatroom'])
+            ->name('chatrooms.enter');
+
+        Route::delete('{chatroomId}/leave', [ChatroomController::class, 'leaveChatroom'])
+            ->name('chatrooms.leave');
+
+        Route::prefix('{chatroomId}/messages')->group(function () {
+            Route::post('/', [MessageController::class, 'sendMessage'])
+                ->name('messages.send');
+
+            Route::get('/', [MessageController::class, 'listMessages'])
+                ->name('messages.list');
+        });
+    });
 });
